246cbfbcacb10c2dfb4a6d75134460d370646675
Travis <travis.williams94@gmail.com>
2023-07-26 14:27:28 -0600

    compelte lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,33 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response(
+            'No Content',
+            204
+        )
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        for key in request.json:
+            setattr(plant, key, request.json[key])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(
+            plant.to_dict(),
+            200
+        )
+
+
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
